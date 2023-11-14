e2763c6c2b942467ecd245d321e88a580ca10a1c
Tavo Cruz <madecruz2@gmail.com>
2023-11-14 13:46:21 -0800

    Start react app with webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const child1 = React.createElement('p', {}, "I am a child1")
 const child2 = React.createElement('p', {}, "I am a child2")
 // const div = React.createElement('div', { className: "text" }, "Hello from react");
