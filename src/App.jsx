a1427a390a28f73b96af4b51adc9b766de3cb05a
Tavo Cruz <madecruz2@gmail.com>
2023-11-15 14:44:44 -0800

    render data from list and importance of keys

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import Button from "./Button.jsx";
-import React from "react";
+import Text from "./Text.jsx";
+import React, {useState} from "react";
 const App = () => {
-  const handleClickAction = () => {
-    console.log("I was clicked from the parent");
+
+  const [data, setData] = useState([
+    { id: "1", text: "a" },
+    { id: "2", text: 'b' },
+    { id: "3", text: 'c' },
+    { id: "4", text: 'd' },
+    { id: "5", text: 'e' }
+  ])
+
+  const addMoreData = () => {
+    setData(prev => [
+      ...prev,
+      {id: "6", text: "f" }
+    ])
   }
-  return <Button
-    clickAction={handleClickAction}
-  >Changed name</Button>
+  return <>
+    {
+      data.map((item, index) => (
+        <Text key={item.id}>{ item.text }</Text>
+      ))
+    }
+    <Button clickAction={addMoreData}>Add More Data</Button>
+  </>
 }
 
 export {App};
--- src/Button.jsx
+++ src/Button.jsx
@@ -4,7 +4,7 @@ const Button = (props) => {
   const { children, clickAction } = props;
   //this makes the click listener local what about when we have a large set of files and we want to share the component and handle the clickable listener on their side when we make use of it ? 
   const handleClick = () => {
-    clickAction()
+    clickAction?.()
   }
   return (
     <button
