2ea45923a0a8cd28c7414abbef68cb55c58b6699
Tavo Cruz <madecruz2@gmail.com>
2023-11-16 20:53:23 -0800

    useEffect, passing props to dependency array

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,15 @@ import React, {useCallback, useEffect, useState} from "react";
 const App = () => {
   const [data1, setData1] = useState(0);
   const [data2, setData2] = useState(0);
-  useEffect(() => {
-    console.log("Data1 changed");
-  }, [data1])
-  
-  useEffect(() => {
-    console.log("Data2 changed");
-  },[data2])
 
   return <>
-    {data1}
+    <Text externalData = {data1}></Text>
     <button onClick={() => setData1(prev => {
       return prev+1;
     })}>Update 1</button>
     <br />
     <br />
-    {data2}
+    <Text externalData = {data2}></Text>
     <button onClick={() => setData2(prev => {
       return prev+1;
     })}>Update 2</button>
